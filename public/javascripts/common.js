bfefc64648a2c849268ee743b884ecf2c8f550e6
Kabosu <249842959@qq.com>
2017-05-10 15:47:44 +0800

    5.10 15:47 -k

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -42,7 +42,7 @@ $('.nav_search i').hover(function(){
 })
 
 
-$('.nav_one').css('border-bottom','2px #C0A75E solid')
+$('.lxm_nav_first').css('border-bottom','2px #C0A75E solid')
 //style.borderBottom = '2px #C0A75E solid'
 $('.nav_list > ul').find('.nav_one').click(function(){
 	var index = $(this).index('.nav_one')
