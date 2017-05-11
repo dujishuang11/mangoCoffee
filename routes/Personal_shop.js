41acfe21c2d8dea22d83ec823cf20f802515f050
Kabosu <249842959@qq.com>
2017-05-11 09:33:34 +0800

    5.11 9:33 -k

--- routes/Personal_shop.js
+++ routes/Personal_shop.js
@@ -23,37 +23,38 @@ router.all('*', function(req, res, next) {
 });
 
 
-function getAllUsers(callback){
-	pool.getConnection(function(err,con){
-		var sql = 'select * from settled';
-		con.query(sql,function(err,result){
-//			console.log('result:'+result)
-			if(err){ 
-				console.log("getAllUsers Error:" + err.message);
-				return;
-			}
-			con.release()//释放连接
-			callback(err,result);
-		})
-	})
-}
+//function getAllUsers(callback){
+//	pool.getConnection(function(err,con){
+//		var sql = 'select * from settled';
+//		con.query(sql,function(err,result){
+////			console.log('result:'+result)
+//			if(err){ 
+//				console.log("getAllUsers Error:" + err.message);
+//				return;
+//			}
+//			con.release()//释放连接
+//			callback(err,result);
+//		})
+//	})
+//}
+//
+//router.get('/list',function(req,res){
+//	console.log('cat');
+//	getAllUsers(function(err,results){
+//		if(err){
+//			res.send(err);
+//		}else if(results){
+//			res.send(results);
+//		}
+//	})
+//})
 
-router.get('/list',function(req,res){
-	console.log('cat');
-	getAllUsers(function(err,results){
-		if(err){
-			res.send(err);
-		}else if(results){
-			res.send(results);
-		}
-	})
-})
 
-function save(username,pass,phone,callback){
+//根据用户名查询用户
+function getAllUsers(shopName,callback){
 	pool.getConnection(function(err,con){
-//		var sql = 'select * from user where username = ?';
-		var sql = 'insert into user(username,pass,phone) value(?,?,?)'
-		con.query(sql,[username,pass,phone],function(err,result){
+		var sql = 'select * from settled where shopName = ?';
+		con.query(sql,[shopName],function(err,result){
 			console.log('result:'+result)
 			if(err){
 				console.log("getAllUsers Error:" + err.message);
@@ -64,6 +65,55 @@ function save(username,pass,phone,callback){
 		})
 	})
 }
+
+
+function save(shopName,shopType,realName,email,phone,qq,briefIntroduction,address,userID,secretKey,examine,callback){
+	pool.getConnection(function(err,con){
+//		var sql = 'select * from user where username = ?';
+		var sql = 'insert into settled(shopName,shopType,realName,email,phone,qq,briefIntroduction,address,userID,secretKey,examine) value(?,?,?,?,?,?,?,?,?,?,?)'
+		con.query(sql,[shopName,shopType,realName,email,phone,qq,briefIntroduction,address,userID,secretKey,examine],function(err,result){
+			console.log('result:'+result)
+			if(err){
+				console.log("getAllUsers Error:" + err.message);
+				return;
+			}
+			con.release()//释放连接
+			callback(err,result);
+		})
+	})
+}
+
+router.post('/shop_register',function(req,res){//请求数据　响应数据
+//	console.log('wtf')
+//	var username = req.body.username;
+	var shopName = '笑笑';
+	var shopType = '摄影师';
+	var realName = '孙亚龙';
+	var email = '249842959@qq.com';
+	var phone = '17600022763';
+	var qq = '249842959';
+	var briefIntroduction = '专业摄影，国际摄影二等奖';
+	var address = '北京市大兴区';
+	var userID = '140622198711260037';
+	var secretKey = '';
+	var examine = 0;
+//	console.log(username)
+	getAllUsers(shopName,function(err,result){
+		if(result == '' || result == null) {
+			console.log('未注册')
+			save(shopName,shopType,realName,email,phone,qq,briefIntroduction,address,userID,secretKey,examine,function(err,result){
+////				if(result.insertId > 0){
+				res.send({flag:1,result:result})
+////				}
+			})
+		}else if(result !='' || result!=null) {
+			res.send({flag:2})
+			console.log('已有')
+		}else {
+			res.send({flag:3})
+		}
+	})
+})
 
 
 module.exports = router;
